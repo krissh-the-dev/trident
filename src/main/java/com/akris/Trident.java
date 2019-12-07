16047cab9818922416b1c3a74a7e59246f5037e5
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2019-12-07 10:08:27 +0530

    switched to maven project structure

--- src/main/java/com/akris/Trident.java
+++ src/main/java/com/akris/Trident.java
@@ -1,38 +1,60 @@
 
 // * AWT ELEMENTS
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.HeadlessException;
+import java.awt.Toolkit;
+
+// * CLIPBOARD ELEMENTS
+
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-
-// * CLIPBOARD ELEMENTS AND UNDO HANDLERS
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.StringSelection;
-import javax.swing.undo.CannotRedoException;
-import javax.swing.undo.CannotUndoException;
-import javax.swing.undo.UndoManager;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.UnsupportedFlavorException;
-
-// * SWING ELEMENTS
-import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-import javax.swing.event.DocumentListener;
-import javax.swing.event.CaretEvent;
-import javax.swing.event.CaretListener;
-import javax.swing.event.DocumentEvent;
-import javax.swing.filechooser.*;
-import javax.swing.text.BadLocationException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 
 // * IO ELEMENTS
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.BufferedWriter;
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Date;
+
+// * SWING ELEMENTS
+import javax.swing.ImageIcon;
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.KeyStroke;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.filechooser.FileSystemView;
+import javax.swing.text.BadLocationException;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+import javax.swing.undo.UndoManager;
+
+import org.json.simple.JSONObject;
 
 class Trident {
   protected static JTextArea textarea;
@@ -121,7 +143,7 @@ class Trident {
         fileType = " File";
         textarea = new JTextArea();
         mb = new JMenuBar();
-        configFilePath = "configurations.json";
+        configFilePath = "com\\akris\\configurations.json";
         path = System.getProperty("java.io.tmpdir") + "New File";
       }
 
@@ -136,7 +158,7 @@ class Trident {
         frame.setResizable(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
-        ImageIcon ic = new ImageIcon("raw\\trident.png");
+        ImageIcon ic = new ImageIcon("com\\akris\\raw\\trident.png");
         frame.setIconImage(ic.getImage());
       }
 
@@ -537,7 +559,7 @@ class FormatMenuListener extends FileMenuListener implements ActionListener {
         jsonEditor.setSize(450, 350);
         jsonEditor.setTitle("Style Editor");
         JPanel TextViewer = new JPanel();
-        File jsonFile = new File("configurations.json");
+        File jsonFile = new File("com\\akris\\configurations.json");
         FileReader fr = new FileReader(jsonFile);
         BufferedReader br = new BufferedReader(fr);
         String jsonContents = "";
@@ -559,7 +581,7 @@ class FormatMenuListener extends FileMenuListener implements ActionListener {
             try {
               // ! Huge delay in editor since we're writing file for every single change
               String jsonContents = jsonViewer.getText();
-              File jsonFile = new File("configurations.json");
+              File jsonFile = new File("com\\akris\\configurations.json");
               FileWriter fileWritter = new FileWriter(jsonFile, false);
               BufferedWriter bw = new BufferedWriter(fileWritter);
               bw.write(jsonContents);
@@ -616,11 +638,11 @@ class AboutMenuListener extends Trident implements ActionListener {
       // TODO: Add link to version.config
       JDialog aboutDialog = new JDialog(frame, "About Trident");
       JPanel infoPanel = new JPanel();
-      ImageIcon ic = new ImageIcon("raw\\trident_logo.png");
+      ImageIcon ic = new ImageIcon("com\\akris\\raw\\trident_logo.png");
       JLabel icon = new JLabel(ic);
       icon.setSize(50, 50);
       JLabel l1 = new JLabel(
-          "<html> <center><h2> <br/>Trident Text Editor</h2> <br/> Version 0.0.5 <br/>ALPHA<br/> <a href=\"https://github.com/KrishnaMoorthy12/trident\">View Source Code - GitHub</a></center> </html>");
+          "<html> <center><h2> <br/>Trident Text Editor</h2> <br/> Version 0.0.6 <br/>MAVEN - ALPHA<br/> <a href=\"https://github.com/KrishnaMoorthy12/trident\">View Source Code - GitHub</a></center> </html>");
       // ! Link is not clickable
       infoPanel.setBorder(new EmptyBorder(10, 5, 5, 5));
       infoPanel.add(icon);
