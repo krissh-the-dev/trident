e346566f372518f7fed4edd9321fb62f5b7a04e1
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2019-12-11 11:41:36 +0530

    added responsive edit menu and pop-up menu

--- trident.java
+++ trident.java
@@ -293,6 +293,9 @@ class Trident {
       ClipMenu.add(EraseClipboard);
       EraseClipboard.addActionListener(eml);
 
+      Thread eal = new EditActionsListener();
+      eal.start();
+
       // < Edit Menu
 
       // > Format Menu
@@ -942,6 +945,62 @@ class CommentPaneListener extends Thread {
       Trident.ErrorDialog("STATUS_THREAD_KILLED", strangeException);
     } catch (Exception died) {
       died.printStackTrace();
+    }
+  }
+}
+
+class EditActionsListener extends Thread {
+  @Override
+  public void run() {
+    try {
+      while (true) {
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        String contents = clipboard.getData(DataFlavor.stringFlavor).toString();
+        try {
+          if (contents.equals("")) {
+            Trident.Paste.setEnabled(false);
+            Trident.pPaste.setEnabled(false);
+            Trident.ShowClipboard.setEnabled(false);
+            Trident.EraseClipboard.setEnabled(false);
+          } else {
+            Trident.pPaste.setEnabled(true);
+            Trident.Paste.setEnabled(true);
+            Trident.ShowClipboard.setEnabled(true);
+            Trident.EraseClipboard.setEnabled(true);
+          }
+        } catch (NullPointerException npe) {
+          Trident.Paste.setEnabled(false);
+          Trident.pPaste.setEnabled(false);
+          Trident.ShowClipboard.setEnabled(false);
+          Trident.EraseClipboard.setEnabled(false);
+        }
+
+        try {
+          if (Trident.textarea.getSelectedText().equals("")) {
+            Trident.Copy.setEnabled(false);
+            Trident.pCopy.setEnabled(false);
+            Trident.Cut.setEnabled(false);
+            Trident.pCut.setEnabled(false);
+          } else {
+            Trident.Copy.setEnabled(true);
+            Trident.pCopy.setEnabled(true);
+            Trident.Cut.setEnabled(true);
+            Trident.pCut.setEnabled(true);
+          }
+        } catch (NullPointerException npe) {
+          Trident.Copy.setEnabled(false);
+          Trident.pCopy.setEnabled(false);
+          Trident.Cut.setEnabled(false);
+          Trident.pCut.setEnabled(false);
+        }
+        Thread.sleep(100);
+      }
+    } catch (InterruptedException inte) {
+      Trident.ErrorDialog("EAL_INTERRUPTION", inte);
+    } catch (UnsupportedFlavorException ufe) {
+      Trident.ErrorDialog("EAL_FLAVOR_ERR", ufe);
+    } catch (Exception some) {
+      Trident.ErrorDialog("UNKNOWN_ERR_EAL", some);
     }
   }
 }
