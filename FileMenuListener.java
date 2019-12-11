4facf2f255f7926f9a7829d295285f1473107ab1
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2019-12-11 15:53:37 +0530

    Organized imports

--- FileMenuListener.java
+++ FileMenuListener.java
@@ -1,66 +1,16 @@
 
-// * AWT ELEMENTS
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Cursor;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.HeadlessException;
-import java.awt.Toolkit;
-import java.awt.Desktop;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.WindowListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
-// * CLIPBOARD ELEMENTS AND UNDO HANDLERS
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.StringSelection;
-import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.MouseListener;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-
-import java.lang.ProcessBuilder.Redirect;
-
-// * IO ELEMENTS
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.net.URL;
 import java.nio.file.Paths;
-import java.time.LocalDateTime;
-import java.util.Date;
-
-// * SWING ELEMENTS
 import javax.swing.ImageIcon;
-import javax.swing.JDialog;
 import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JPopupMenu;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.KeyStroke;
-import javax.swing.SwingConstants;
-import javax.swing.UIManager;
-import javax.swing.border.EmptyBorder;
-import javax.swing.event.CaretEvent;
-import javax.swing.event.CaretListener;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
 import javax.swing.filechooser.FileSystemView;
 
 class FileMenuListener extends Trident implements ActionListener {
--- trident.java
+++ trident.java
@@ -1,45 +1,27 @@
+// * Listeners
 
-// * AWT ELEMENTS
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Cursor;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.HeadlessException;
-import java.awt.Toolkit;
-import java.awt.Desktop;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseAdapter;
 import java.awt.event.WindowListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-
-// * CLIPBOARD ELEMENTS AND UNDO HANDLERS
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.StringSelection;
-import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseListener;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-
-import java.lang.ProcessBuilder.Redirect;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 // * IO ELEMENTS
+
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
-import java.net.URL;
+import java.io.BufferedWriter;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
-import java.util.Date;
 
-// * SWING ELEMENTS
+// * UI Elements
+
 import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 import javax.swing.JFileChooser;
@@ -57,15 +39,26 @@ import javax.swing.KeyStroke;
 import javax.swing.SwingConstants;
 import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
-import javax.swing.event.CaretEvent;
-import javax.swing.event.CaretListener;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+// *Misc
+
+import javax.swing.undo.UndoManager;
+import java.net.URL;
+import java.awt.Desktop;
+
+// * Exceptions
 
 import javax.swing.text.BadLocationException;
 import javax.swing.undo.CannotRedoException;
+import java.awt.HeadlessException;
 import javax.swing.undo.CannotUndoException;
-import javax.swing.undo.UndoManager;
+import java.io.IOException;
 
 class Trident {
   protected static JTextArea textarea;
@@ -240,6 +233,7 @@ class Trident {
           }
         }
       };
+
       frame.addWindowListener(WindowCloseListener);
       frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
       frame.setLayout(new BorderLayout());
@@ -445,9 +439,7 @@ class Trident {
       frame.getContentPane().add(statusBar, BorderLayout.SOUTH);
       frame.setVisible(true);
 
-    } catch (
-
-    Exception e) {
+    } catch (Exception e) {
       ErrorDialog("MAIN_THREAD_CRASH", e);
       System.err.println("Unexpected crash...");
       e.printStackTrace();
