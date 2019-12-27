ecb1e4b08ae832d900941e51feece8c61a924343
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2019-12-27 17:21:50 +0530

    The Find and Replace

--- FindAndReplace.java
+++ FindAndReplace.java
@@ -4,6 +4,9 @@ import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
@@ -13,12 +16,17 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.ArrayList;
+
 class FindAndReplace {
   public static JDialog findDialog, replaceDialog;
-  public static JButton findButton, findAllButton, replaceButton, replaceAllButton, closeButton;
+  public static JButton findButton, findNextButton, replaceButton, replaceAllButton, closeButton;
   public static JTextField findField, replaceField;
   public static JCheckBox matchCase, wholeWords;
   static boolean fimOpen, rimOpen;
+  static FindReplaceButtonsListener frbl = new FindReplaceButtonsListener();
 
   public static void findUI() {
     if (fimOpen) {
@@ -35,19 +43,36 @@ class FindAndReplace {
     contentPanel.setLayout(new BorderLayout(5, 5));
 
     JLabel findLabel = new JLabel("Find ");
-    findField = new JTextField(40);
-    JPanel mainPanel = new JPanel(new FlowLayout());
-    mainPanel.add(findLabel);
-    mainPanel.add(findField);
+    findField = new JTextField(43);
+    // findField.setComponentPopupMenu(Trident.editorMenu);
+    JPanel mainPanel = new JPanel(new GridLayout(2, 1, 1, 1));
+    JPanel fieldPane = new JPanel();
+    fieldPane.setBorder(new EmptyBorder(5, 0, 0, 0));
+    fieldPane.add(findLabel);
+    fieldPane.add(findField);
+    mainPanel.add(fieldPane);
+
+    wholeWords = new JCheckBox("Whole Words only");
+    matchCase = new JCheckBox("Match Case");
+    JPanel opsPane = new JPanel(new GridLayout(1, 2, 1, 1));
+    opsPane.setBorder(new TitledBorder(new EtchedBorder(), "Options", 0, TitledBorder.CENTER));
+    opsPane.add(wholeWords);
+    opsPane.add(matchCase);
+    mainPanel.add(opsPane);
 
     findButton = new JButton("Find");
-    findAllButton = new JButton("Find All");
+    findButton.addActionListener(frbl);
+    findNextButton = new JButton("Find Next");
+    findNextButton.addActionListener(frbl);
     closeButton = new JButton("Close");
+    closeButton.addActionListener(frbl);
+
     JPanel buttonPanel = new JPanel(new FlowLayout());
     buttonPanel.add(findButton);
-    buttonPanel.add(findAllButton);
+    buttonPanel.add(findNextButton);
     buttonPanel.add(closeButton);
 
+    contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
     contentPanel.add(mainPanel, BorderLayout.NORTH);
     contentPanel.add(buttonPanel, BorderLayout.SOUTH);
     findDialog.add(contentPanel);
@@ -60,7 +85,7 @@ class FindAndReplace {
       }
     });
     closeButton.addActionListener(new FindReplaceButtonsListener());
-    findDialog.setSize(400, 100);
+    findDialog.setSize(400, 165);
     findDialog.setLocationRelativeTo(Trident.frame);
     findDialog.setResizable(false);
     findDialog.setVisible(true);
@@ -83,7 +108,7 @@ class FindAndReplace {
     findField = new JTextField(40);
     JLabel replaceLabel = new JLabel("Replace ");
     replaceField = new JTextField(38);
-    JPanel mainPanel = new JPanel(new GridLayout(3, 1, 1, 1));
+    JPanel mainPanel = new JPanel(new GridLayout(2, 1, 1, 1));
     JPanel fPane = new JPanel(new FlowLayout());
     JPanel rPane = new JPanel(new FlowLayout());
     fPane.add(findLabel);
@@ -93,20 +118,42 @@ class FindAndReplace {
     mainPanel.add(fPane);
     mainPanel.add(rPane);
 
+    wholeWords = new JCheckBox("Whole Words only");
+    matchCase = new JCheckBox("Match Case");
+    JPanel opsPane = new JPanel(new GridLayout(1, 2, 1, 1));
+    opsPane.setBorder(new TitledBorder(new EtchedBorder(), "Options", 0, TitledBorder.CENTER));
+    opsPane.add(wholeWords);
+    opsPane.add(matchCase);
+    // mainPanel.add(opsPane);
+
     findButton = new JButton("Find");
-    findAllButton = new JButton("Find All");
+    findButton.addActionListener(frbl);
+
+    findNextButton = new JButton("Find Next");
+    findNextButton.addActionListener(frbl);
+
     replaceButton = new JButton("Replace");
+    replaceButton.addActionListener(frbl);
+
     replaceAllButton = new JButton("Replace All");
+    replaceAllButton.addActionListener(frbl);
+
     closeButton = new JButton("Close");
+    closeButton.addActionListener(frbl);
+
     JPanel buttonPanel = new JPanel(new FlowLayout());
     buttonPanel.add(findButton);
-    buttonPanel.add(findAllButton);
+    buttonPanel.add(findNextButton);
     buttonPanel.add(replaceButton);
     buttonPanel.add(replaceAllButton);
     buttonPanel.add(closeButton);
 
-    mainPanel.add(buttonPanel);
-    replaceDialog.add(mainPanel);
+    contentPanel.setLayout(new BorderLayout());
+    contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+    contentPanel.add(mainPanel, BorderLayout.NORTH);
+    contentPanel.add(opsPane, BorderLayout.CENTER);
+    contentPanel.add(buttonPanel, BorderLayout.SOUTH);
+    replaceDialog.add(contentPanel);
     replaceDialog.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
     replaceDialog.addWindowListener(new WindowAdapter() {
       @Override
@@ -116,7 +163,7 @@ class FindAndReplace {
       }
     });
     closeButton.addActionListener(new FindReplaceButtonsListener());
-    replaceDialog.setSize(400, 150);
+    replaceDialog.setSize(400, 195);
     replaceDialog.setLocationRelativeTo(Trident.frame);
     replaceDialog.setResizable(false);
     replaceDialog.setVisible(true);
@@ -124,27 +171,100 @@ class FindAndReplace {
 }
 
 class FindReplaceButtonsListener implements ActionListener {
+  static boolean found = false;
+  static ArrayList<Integer> foundStarts;
+  static ArrayList<Integer> foundEnds;
+
+  public static void find() {
+    foundStarts = new ArrayList<>();
+    foundEnds = new ArrayList<>();
+    Pattern pattern;
+    if (FindAndReplace.wholeWords.isSelected() && !FindAndReplace.matchCase.isSelected()) {
+      Trident.textarea.setCaretPosition(Trident.textarea.getCaretPosition());
+      pattern = Pattern.compile("\\b" + FindAndReplace.findField.getText() + "\\b");
+    } else if (FindAndReplace.wholeWords.isSelected() && FindAndReplace.matchCase.isSelected()) {
+      Trident.textarea.setCaretPosition(Trident.textarea.getCaretPosition());
+      pattern = Pattern.compile("\\b" + FindAndReplace.findField.getText() + "\\b", Pattern.CASE_INSENSITIVE);
+    } else if (!FindAndReplace.wholeWords.isSelected() && !FindAndReplace.matchCase.isSelected()) {
+      pattern = Pattern.compile(FindAndReplace.findField.getText(), Pattern.CASE_INSENSITIVE);
+    } else
+      pattern = Pattern.compile(FindAndReplace.findField.getText());
+    Matcher matcher = pattern.matcher(Trident.textarea.getText());
+    while (matcher.find()) {
+      foundStarts.add(matcher.start());
+      foundEnds.add(matcher.end());
+      Trident.status1.setText("Found " + foundStarts.size() + " matches.");
+      found = true;
+    }
+    if (!found) {
+      Trident.status1.setText("No match found.");
+    }
+  }
+
+  static int i = 0;
+
+  public static void findNext() {
+    find();
+    try {
+      Trident.textarea.setSelectionStart(foundStarts.get(i));
+      Trident.textarea.setSelectionEnd(foundEnds.get(i));
+      i++;
+      if (i == foundEnds.size()) {
+        i = 0;
+      }
+    } catch (IndexOutOfBoundsException iob) {
+      Trident.status1.setText("No more matchs found");
+    }
+  }
+
+  public static void replace() {
+    if (foundStarts.size() == 0)
+      return;
+    String replacement = FindAndReplace.replaceField.getText();
+    Trident.textarea.replaceSelection(replacement);
+    Trident.status1.setText("One occurance was replaced.");
+    find();
+    findNext();
+  }
+
+  public static void replaceAll() {
+    find();
+    for (int i = 0; i < foundStarts.size(); i++) {
+      find();
+      replace();
+    }
+    replace();
+  }
+
   @Override
   public void actionPerformed(ActionEvent ae) {
     switch (ae.getActionCommand()) {
     case "Find":
+      find();
       break;
 
-    case "Find All":
+    case "Find Next":
+      findNext();
       break;
 
     case "Replace":
+      find();
+      replace();
       break;
 
     case "Replace All":
+      replaceAll();
       break;
 
     case "Close":
-      FindAndReplace.fimOpen = false;
-      FindAndReplace.findDialog.dispose();
-
-      FindAndReplace.rimOpen = false;
-      FindAndReplace.replaceDialog.dispose();
+      if (FindAndReplace.fimOpen) {
+        FindAndReplace.fimOpen = false;
+        FindAndReplace.findDialog.dispose();
+      }
+      if (FindAndReplace.rimOpen) {
+        FindAndReplace.rimOpen = false;
+        FindAndReplace.replaceDialog.dispose();
+      }
       break;
     }
   }
