93135f2abbaded9dfdb38299bb37da0af54c5854
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2020-01-24 19:42:31 +0530

    Revert "update readme" (wrong commit)
    This reverts commit 518dacc58e9de1b2333ff50d6a87ac08c8fa5061.

--- FileMenuListener.java
+++ FileMenuListener.java
@@ -307,8 +307,6 @@ class FileMenuListener implements ActionListener {
 
       case "New Window":
         new Trident("New File");
-        ProcessBuilder pb = new ProcessBuilder("Trident.bat");
-        Process p = pb.start();
         break;
 
       case "Open":
