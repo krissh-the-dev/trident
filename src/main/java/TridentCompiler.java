0fb0d07e8dbe4351e38316b0efabcf37d0898914
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-03-25 11:53:54 +0530

    added terminal launcher for linux

--- src/main/java/TridentCompiler.java
+++ src/main/java/TridentCompiler.java
@@ -49,31 +49,31 @@ public class TridentCompiler {
     }
     String fileType = FileTypeParser.getType(filepath);
     switch (fileType) {
-    case "Python Source File":
-      Runtime.getRuntime().exec("cmd /c start cmd.exe /K"  + "\"title Trident Compiler && echo && python \""  + filepath
-           + "\" && pause && exit \"");
-      break;
+      case "Python Source File":
+        Runtime.getRuntime().exec("cmd /c start cmd.exe /K" + "\"title Trident Compiler && echo && python \"" + filepath
+            + "\" && pause && exit \"");
+        break;
 
-    case "Java Source File":
-      Runtime.getRuntime().exec("cmd /c start cmd.exe /K"  + "\"title Trident Compiler && echo && javac \""  + filepath
-           + "\" && echo Compilation ended. && pause && exit \"");
-      break;
+      case "Java Source File":
+        Runtime.getRuntime().exec("cmd /c start cmd.exe /K" + "\"title Trident Compiler && echo && javac \"" + filepath
+            + "\" && echo Compilation ended. && pause && exit \"");
+        break;
 
-    case "C Source File":
-      String fileLocation = (new File(filepath)).getParent();
-      Runtime.getRuntime().exec("cmd /c start cmd.exe /K"  + "\"title Trident Compiler && cd \""  + fileLocation
-           + "\" && echo && gcc \""  + filepath  + "\" -std=c99 && echo Compilation ended. && pause && exit \"");
-      // using C99 to avoid irritating forbidden errors
-      break;
+      case "C Source File":
+        String fileLocation = (new File(filepath)).getParent();
+        Runtime.getRuntime().exec("cmd /c start cmd.exe /K" + "\"title Trident Compiler && cd \"" + fileLocation
+            + "\" && echo && gcc \"" + filepath + "\" -std=c99 && echo Compilation ended. && pause && exit \"");
+        // using C99 to avoid irritating forbidden errors
+        break;
 
-    case "C++ Source File":
-      fileLocation = (new File(filepath)).getParent();
-      Runtime.getRuntime().exec("cmd /c start cmd.exe /K"  + "\"title Trident Compiler && cd \""  + fileLocation
-           + "\" && echo && g++ \""  + filepath  + "\" && echo Compilation ended. && pause && exit \"");
-      break;
+      case "C++ Source File":
+        fileLocation = (new File(filepath)).getParent();
+        Runtime.getRuntime().exec("cmd /c start cmd.exe /K" + "\"title Trident Compiler && cd \"" + fileLocation
+            + "\" && echo && g++ \"" + filepath + "\" && echo Compilation ended. && pause && exit \"");
+        break;
 
-    default:
-      throw new UnsupportedFileException(filepath);
+      default:
+        throw new UnsupportedFileException(filepath);
     }
   }
 
@@ -93,46 +93,46 @@ public class TridentCompiler {
     Process p = null;
 
     if (filepath.contains("powerboil")) {
-      p = Runtime.getRuntime().exec("cmd /c start cmd.exe /K"  + "\"title Trident - PowerBoil Alpha && echo && python \""
-           + filepath  + "\" && pause && exit \"");
+      p = Runtime.getRuntime().exec("cmd /c start cmd.exe /K" + "\"title Trident - PowerBoil Alpha && echo && python \""
+          + filepath + "\" && pause && exit \"");
       return;
     }
 
     String fileType = FileTypeParser.getType(filepath);
     switch (fileType) {
-    case "Python Source File":
-      p = Runtime.getRuntime().exec("cmd /c start cmd.exe /K"  + "\"title Trident Compiler && echo && python \""
-           + filepath  + "\" && pause && exit \"");
-      break;
+      case "Python Source File":
+        p = Runtime.getRuntime().exec("cmd /c start cmd.exe /K" + "\"title Trident Compiler && echo && python \""
+            + filepath + "\" && pause && exit \"");
+        break;
 
-    case "Java Source File":
-    case "Java Class File":
-      Path name = Paths.get(filepath);
-      String classFile = name.getFileName().toString();
-      exec = classFile.replaceFirst("[.][^.] + $", "");
-      String location = new File(filepath).getParent().toString();
-      p = Runtime.getRuntime().exec("cmd /c start cmd.exe /K"  + "\"title Trident Compiler &&  cd \""  + location
-           + "\" && echo && java "  + exec  + " && pause && exit \"");
-      break;
+      case "Java Source File":
+      case "Java Class File":
+        Path name = Paths.get(filepath);
+        String classFile = name.getFileName().toString();
+        exec = classFile.replaceFirst("[.][^.] + $", "");
+        String location = new File(filepath).getParent().toString();
+        p = Runtime.getRuntime().exec("cmd /c start cmd.exe /K" + "\"title Trident Compiler &&  cd \"" + location
+            + "\" && echo && java " + exec + " && pause && exit \"");
+        break;
 
-    case "C Source File":
-    case "C++ Source File":
-      exec = (new File(filepath)).getParent()  + "/a.exe";
-      p = Runtime.getRuntime()
-          .exec("cmd /c start cmd.exe /K"  + "\"title Trident Compiler && echo && \""  + exec  + "\" && pause && exit \"");
-      break;
+      case "C Source File":
+      case "C++ Source File":
+        exec = (new File(filepath)).getParent() + "/a.exe";
+        p = Runtime.getRuntime().exec(
+            "cmd /c start cmd.exe /K" + "\"title Trident Compiler && echo && \"" + exec + "\" && pause && exit \"");
+        break;
 
-    case "HTML File":
-      try {
-        filepath = filepath.replace('\\', '/');
-        Desktop.getDesktop().browse(java.net.URI.create("file:///"  + filepath));
-      } catch (IOException ioe) {
-        Trident.ErrorDialog("COMPILER_BROSWER_ERR", ioe);
-      }
-      break;
+      case "HTML File":
+        try {
+          filepath = filepath.replace('\\', '/');
+          Desktop.getDesktop().browse(java.net.URI.create("file:///" + filepath));
+        } catch (IOException ioe) {
+          Trident.ErrorDialog("COMPILER_BROSWER_ERR", ioe);
+        }
+        break;
 
-    default:
-      throw new UnsupportedFileException(filepath);
+      default:
+        throw new UnsupportedFileException(filepath);
     }
     p.waitFor();
     p.destroy();
@@ -145,13 +145,12 @@ public class TridentCompiler {
      * @throws: Unsupported OS Exception, if OS is not supported for this feature
      */
     try {
+      String parent = (new File(Trident.path)).getParent();
       if (os == 1) {
-        String parent = (new File(Trident.path)).getParent();
         Runtime.getRuntime()
-            .exec("cmd /c start cmd.exe /K"  + "\"title Trident Compiler Console && cd "  + parent  + "\"");
+            .exec("cmd /c start cmd.exe /K" + "\"title Trident Compiler Console && cd " + parent + "\"");
       } else if (os == 2) {
-        String[] processArgs = new String[] { "/bin/bash", "-c", "Start" };
-        new ProcessBuilder(processArgs).start();
+        Runtime.getRuntime().exec("/usr/bin/gnome-terminal  && cd \"" + parent + "\"");
       } else
         throw new UnsupportedOperatingSystemException();
     } catch (UnsupportedOperatingSystemException unOS) {
