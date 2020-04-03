73895c130f2b6059d87c48b09960ea360be59cee
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2020-04-03 16:53:31 +0530

    added recently opened tracker [1/3]

---
+++ src/main/java/RecentsTracker.java
@@ -0,0 +1,50 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+public class RecentsTracker {
+  public static ArrayList<String> recents = new ArrayList<>();
+
+  RecentsTracker() {
+    readRecords();
+  }
+
+  @SuppressWarnings("unchecked")
+  public static void readRecords() {
+    try {
+      if (!(new File("logs/recents.tcf")).exists()) {
+        (new File("logs/recents.tcf")).createNewFile();
+        System.out.println("New file created.");
+      }
+      ObjectInputStream ois = new ObjectInputStream(new FileInputStream("logs/recents.tcf"));
+      recents = (ArrayList<String>) ois.readObject();
+      ois.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void addRecord(String filePath) {
+    readRecords();
+    if (recents.size() >= 5) {
+      recents.remove(0);
+    }
+    recents.add(filePath);
+    try (FileOutputStream fos = new FileOutputStream("logs/recents.tcf");
+        ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+      oos.writeObject(recents);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void printRecords() {
+    readRecords();
+    for (String x : recents) {
+      System.out.println(x);
+    }
+  }
+}
