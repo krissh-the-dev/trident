1c8e8606312277a16f662a25882439b20288618f
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-04-05 14:01:05 +0530

    deprecation supressing in driver

--- src/main/java/TridentDriver.java
+++ src/main/java/TridentDriver.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class TridentDriver {
+  @SuppressWarnings("deprecation")
   public static void main(String[] args) {
     Scanner ns = new Scanner(System.in);
     while (true) {
