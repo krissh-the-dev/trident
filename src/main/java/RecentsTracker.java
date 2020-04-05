c65490d2584fc5527c618871a1c6dc675f06be20
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-04-05 13:57:01 +0530

    added documentated comments

--- src/main/java/RecentsTracker.java
+++ src/main/java/RecentsTracker.java
@@ -1,3 +1,23 @@
+/*
+ *  RecentsTracker.java
+ *  (c) Copyright, 2020 - 2021 Krishna Moorthy
+ *  akrishnamoorthy007@gmail.com | github.com/KrishnaMoorthy12
+ *  
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -6,14 +26,25 @@ import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class RecentsTracker {
+  /**
+   * Contains methods that keeps writes / reads recently opened files in Trident
+   * (ONLY last 5) into a ArrayList Object as tcf file
+   */
   public static ArrayList<String> recents = new ArrayList<>();
 
   RecentsTracker() {
+    /**
+     * Reads the records upon class object initializaton (Not implemented)
+     */
     readRecords();
   }
 
   @SuppressWarnings("unchecked")
   public static void readRecords() {
+    /**
+     * Reads records from the .tcf file and casts it into an ArrayList object
+     * (recents)
+     */
     try {
       if (!(new File("logs/recents.tcf")).exists()) {
         (new File("logs/recents.tcf")).createNewFile();
@@ -29,6 +60,11 @@ public class RecentsTracker {
   }
 
   public static void addRecord(String filePath) {
+    /**
+     * adds a new element to the recents arraylist and then writes to the tcf file
+     * 
+     * @param: path of the file opened (String)
+     */
     readRecords();
     if (recents.size() >= 5) {
       recents.remove(0);
@@ -47,11 +83,18 @@ public class RecentsTracker {
   }
 
   public static ArrayList<String> getRecords() {
+    /**
+     * returns the arraylist of recently opened file paths
+     */
     readRecords();
     return recents;
   }
 
+  @Deprecated
   public static void printRecords() {
+    /**
+     * Prints the records from the arraylist
+     */
     readRecords();
     if (recents.size() == 0) {
       System.out.println("No records found");
