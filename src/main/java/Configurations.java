671b4396f3961f79e4f61b114790bae789ed043b
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-04-07 15:39:20 +0530

    update license headers

--- src/main/java/Configurations.java
+++ src/main/java/Configurations.java
@@ -3,19 +3,17 @@
  *  (c) Copyright, 2020 - 2021 Krishna Moorthy
  *  akrishnamoorthy007@gmail.com | github.com/KrishnaMoorthy12
  *  
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 import java.awt.BorderLayout;
@@ -61,7 +59,7 @@ import mdlaf.themes.MaterialLiteTheme;
  * Trident Configurations Toolkit v2.4
  * @author: Krishna Moorthy
  * 
- * [GPL v3] Trident > Configurations
+ * [Apache v2] Trident > Configurations
  * This is only a minor sub-software of Project Trident
  * Trident Text Editor v3.0 Style Configuration Toolkit
  * Stable since @version 3.0
@@ -69,7 +67,7 @@ import mdlaf.themes.MaterialLiteTheme;
  */
 
 /*
- * (GPL v3) Trident > ChangeListener
+ * (Apache v2) Trident > ChangeListener
  * @author: Krishna Moorthy
  */
 
